35cff12a3920f1fe4cd091717ef4b032ce9449b0
Hotakus <553070703@qq.com>
2021-09-07 19:59:51 +0800

    ...

--- app/src/main/java/com/hotakus/hgma/MainActivity.kt
+++ app/src/main/java/com/hotakus/hgma/MainActivity.kt
@@ -2,7 +2,13 @@ package com.hotakus.hgma
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.animation.AnimationUtils
+import android.view.animation.ScaleAnimation
+import android.view.animation.TranslateAnimation
+import android.widget.ArrayAdapter
+import androidx.core.content.res.ResourcesCompat
 import com.hotakus.hgma.databinding.ActivityMainBinding
+
 
 class MainActivity : AppCompatActivity() {
 
@@ -16,8 +22,15 @@ class MainActivity : AppCompatActivity() {
         bindingInit()
         setContentView(binding.root)
 
-        val bg = binding.MainLinearLayout.background
-        val cf = bg.colorFilter
+        val icon = ResourcesCompat.getDrawable(resources, R.drawable.indicator_led_r, null)
+        icon?.setBounds(0, 0, icon.minimumWidth, icon.minimumHeight)
+        binding.btName.setCompoundDrawables(icon, null, null, null)
+
+        val data = mutableListOf("HelloWorld", "HellGateMonitorBT","HelloWorld", "HellGateMonitorBT", "HelloWorld", "HellGateMonitorBT")
+        binding.btList.adapter = ArrayAdapter(this, R.layout.my_list, data)
+
+
+        val anim = ScaleAnimation(this, null)
 
     }
 }
---
+++ app/src/main/java/com/hotakus/hgma/ToastUtil.kt
@@ -0,0 +1,12 @@
+package com.hotakus.hgma
+
+import android.content.Context
+import android.widget.Toast
+
+fun String.showTaost(context: Context, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(context, this, duration).show()
+}
+
+fun Int.showTaost(context: Context, duration: Int = Toast.LENGTH_SHORT) {
+    Toast.makeText(context, this, duration).show()
+}
